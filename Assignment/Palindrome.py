b3bba5ff6a1fdb5286cb0b3f9584c5fe06d9a73a
MikaMahaputra <55399230+MikaMahaputra@users.noreply.github.com>
2019-09-25 11:51:33 +0700

    Add files via upload

---
+++ Assignment/Palindrome.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 25 11:46:58 2019
+
+@author: user
+"""
+
+import string
+def ignore_punctuation(word):
+    return "",join(letter.lower()for letter in word if letter in string.ascii_letters)
+def reverse(word):
+    return word[::-1]
+def is_palindrome(word):
+    word = ignore_punctuation(word)
+    rev= reverse(word)
+    if (word == rev):
+        return True
+    else:
+        return False
+    
+word= str(input("Enter a word"))
+result= is_palindrome(word)
+
+if result:
+    print("Yes, it's a palindrome)
+else:
+    print("It's not a palindrome")
