7a632939c2301061cdf358de3f8727bcf443d4a3
MikaMahaputra <mikamahaputra@yahoo.co.id>
2020-05-02 00:54:31 +0700

    Inheritance #2 in Java

---
+++ Mr Jude Homework/Semester II/05-01-2020/Inheritance 2/Genus.java
@@ -0,0 +1,18 @@
+
+public class Genus {
+    private String GenusName;
+
+    public Genus(String g){
+        this.GenusName = g;
+    }
+
+    public String getGenusName(){
+        return this.GenusName;
+    }
+
+    public void setGenusName(String g){
+        this.GenusName = GenusName;
+
+    }
+
+}
